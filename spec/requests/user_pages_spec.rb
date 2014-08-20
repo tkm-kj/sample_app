e3443fd08b900b7eba786545771afca1e4565629
ivezuki <ivezuki3@yahoo.co.jp>
2014-08-20 17:43:01 +0900

    finish 9-1

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,4 +48,40 @@ describe "User pages" do
       end
     end
   end
+
+  describe 'edit' do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe 'page' do
+      it { should have_content('Update your profile') }
+      it { should have_title('Edit user') }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe '間違った情報の時' do
+      before { click_button 'Save changes' }
+      it { should have_content('error') }
+    end
+
+    describe '正しい情報の時' do
+      let(:new_name) { 'Hoge' }
+      let(:new_email) { 'hoge@example.com' }
+      before do
+        fill_in 'Name', with: new_name
+        fill_in 'Email', with: new_email
+        fill_in 'Password', with: user.password
+        fill_in 'Confirm Password', with: user.password
+        click_button 'Save changes'
+      end
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
+  end
 end
